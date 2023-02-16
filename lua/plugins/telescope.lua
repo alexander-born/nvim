7ac7f353a041dd42cb3ff3a3b9f00f649e68b7ff
Alexander Born <alexander.born@bmw.de>
2023-02-16 17:39:31 +0100

    Fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,26 +2,9 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      { "nvim-lua/plenary.nvim", lazy = true },
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        config = function()
-          pcall(require("telescope").load_extension, "fzf")
-        end,
-      },
-      {
-        "nvim-telescope/telescope-project.nvim",
-        config = function()
-          require("telescope").load_extension("project")
-        end,
-      },
-      {
-        "AckslD/nvim-neoclip.lua",
-        config = function()
-          require("neoclip").setup()
-        end,
-      },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      { "nvim-telescope/telescope-project.nvim" },
+      { "AckslD/nvim-neoclip.lua" },
     },
     keys = { { "<leader>sa", false } },
     opts = {
@@ -31,5 +14,12 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("project")
+      telescope.load_extension("fzf")
+      require("neoclip").setup()
+    end,
   },
 }
