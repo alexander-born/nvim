2fa6ec77bc98e820eed065d214cecbe328adee54
Alexander Born <alexander.born@bmw.de>
2023-02-16 15:42:12 +0100

    Fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,3 @@
-local function on_project_selected(prompt_bufnr)
-  local available, project_actions = pcall(require, "telescope._extensions.project.actions")
-  if not available then
-    return
-  end
-  local user_actions_available, user_project_actions = pcall(require, "user.project.actions")
-  project_actions.change_working_directory(prompt_bufnr, false)
-  vim.g.project_path = vim.fn.getcwd()
-  if user_actions_available then
-    user_project_actions(prompt_bufnr)
-  end
-end
-
-local function find_project()
-  local available, telescope = pcall(require, "telescope")
-  if not available then
-    return
-  end
-  telescope.extensions.project.project({
-    display_type = "full",
-    attach_mappings = function(prompt_bufnr, map)
-      map({ "n", "i" }, "<CR>", on_project_selected)
-      return true
-    end,
-  })
-end
-
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -50,7 +23,6 @@ return {
         end,
       },
     },
-    keys = { { "<leader>fp", find_project, desc = "Find Plugin File" } },
     opts = {
       extensions = {
         project = {
