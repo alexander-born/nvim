96dbbc397424ef1e02b7204bd3d2cbc98db2332b
Alexander Born <alexander.born@bmw.de>
2023-02-20 16:11:52 +0100

    disable flit

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { "folke/noice.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
 }
