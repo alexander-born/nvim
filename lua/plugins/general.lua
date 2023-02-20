cbbb221213ce4d409c44e3f6d14d8d237f8d34f4
Alexander Born <alexander.born@bmw.de>
2023-02-20 13:04:28 +0100

    formatting change

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,36 +1,12 @@
+-- stylua: ignore
 return {
   { "folke/lazy.nvim", opts = { checker = { notify = false } } },
-  {
-    "gbprod/substitute.nvim",
-    config = function()
-      require("substitute").setup()
-    end,
-  },
+  { "gbprod/substitute.nvim", config = function() require("substitute").setup() end, },
   { "tpope/vim-abolish" },
   { "mg979/vim-visual-multi" },
-  {
-    "vimwiki/vimwiki",
-    config = function()
-      vim.g.vimwiki_list =
-        { {
-          ext = ".md",
-          path = "~/projects/vimwiki/",
-          syntax = "markdown",
-        } }
-    end,
-  },
   { "davidgranstrom/nvim-markdown-preview" },
-  {
-    "aserowy/tmux.nvim",
-    config = function()
-      return require("tmux").setup({ copy_sync = { enable = false } })
-    end,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  },
+  { "aserowy/tmux.nvim", config = function() return require("tmux").setup({ copy_sync = { enable = false } }) end, },
+  { "numToStr/Comment.nvim", config = function() require("Comment").setup() end, },
   { "machakann/vim-sandwich" },
+  { "vimwiki/vimwiki", config = function() vim.g.vimwiki_list = { { ext = ".md", path = "~/projects/vimwiki/", syntax = "markdown", } } end, },
 }
