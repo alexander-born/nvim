b8aa00fb49b6e64195c12efb95f138015131b443
Alexander Born <alexander.born@bmw.de>
2023-02-22 10:34:22 +0100

    add o tree mapping for til

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -9,4 +9,5 @@ return {
   { "vimwiki/vimwiki", config = function() vim.g.vimwiki_list = { { ext = ".md", path = "~/projects/vimwiki/", syntax = "markdown", } } end, },
   { "aserowy/tmux.nvim", config = function() return require("tmux").setup({ copy_sync = { enable = false } }) end, },
   { "numToStr/Comment.nvim", config = function() require("Comment").setup() end, },
+  { "nvim-neo-tree/neo-tree.nvim", opts = { window = { mappings = {["o"] = "open"} } } },
 }
