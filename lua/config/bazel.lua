b2519f70d998c25081ac8ea1f767515c10bd317f
Alexander Born <alexander.born@bmw.de>
2023-02-23 10:25:25 +0100

    better python bazel support

--- lua/config/bazel.lua
+++ lua/config/bazel.lua
@@ -49,8 +49,7 @@ local function get_bazel_python_modules(program)
   return extra_paths
 end
 
-local function get_python_path(program)
-  local extra_paths = get_bazel_python_modules(program)
+local function construct_python_path(extra_paths)
   local env = ""
   local sep = ""
   for _, extra_path in pairs(extra_paths) do
@@ -60,48 +59,69 @@ local function get_python_path(program)
   return env
 end
 
-local function setup_pyright(extra_paths)
-  local config = { capabilities = require("config.lsp").get_capabilities() }
-  config.settings = { python = { analysis = { extraPaths = extra_paths } } }
-  require("lspconfig")["pyright"].setup(config)
+local function get_python_path(program)
+  local extra_paths = get_bazel_python_modules(program)
+  return construct_python_path(extra_paths)
 end
 
-local function add_python_deps_to_pyright(target, workspace)
-  local query = "bazel cquery "
-    .. vim.g.bazel_config
-    .. " '"
-    .. target
-    .. "' --output starlark --starlark:expr='providers(target)[\"PyInfo\"].imports'"
+local function save_pyright_config_json(extra_paths, include)
+  local config = { typeCheckingMode = "off", extraPaths = extra_paths, include = include }
+  local json = { vim.fn.json_encode(config) }
+  vim.fn.writefile(json, "pyrightconfig.json")
+end
 
-  local ws_name = bazel.get_workspace_name(workspace)
-  local function parse_and_add_extra_path(_, stdout)
-    -- local extra_paths = {workspace}
-    local extra_paths = {}
-    local query_output = stdout[1]
-    local depset = query_output:match("depset%(%[(.*)%]")
-    if depset == nil then
-      return
-    end
-    for extra_path in depset:gmatch('"(.-)"') do
-      if extra_path:match("^" .. ws_name) then
-        -- local path = extra_path:gsub("^" .. ws_name, workspace .. "/bazel-bin")
-        local path = extra_path:gsub("^" .. ws_name, workspace)
-        table.insert(extra_paths, path)
-      else
-        table.insert(extra_paths, workspace .. "/external/" .. extra_path)
-      end
-    end
-    vim.pretty_print(extra_paths)
-    setup_pyright(extra_paths)
+local function get_keys(t)
+  local keys = {}
+  for key, _ in pairs(t) do
+    table.insert(keys, key)
   end
+  return keys
+end
 
-  vim.fn.jobstart(query, { on_stdout = parse_and_add_extra_path })
+function M.create_pyright_config(target, include)
+  local workspace = bazel.get_workspace()
+  local ws_name = bazel.get_workspace_name(workspace)
+  local Path = require("plenary.path")
+  local Job = require("plenary.job")
+  Job:new({
+    command = "bazel",
+    args = {
+      "cquery",
+      vim.g.bazel_config,
+      "kind(py_.*," .. target .. ")",
+      "--output",
+      "starlark",
+      "--starlark:expr",
+      'providers(target)["PyInfo"].imports',
+    },
+    on_exit = function(job)
+      local depsets = job:result()
+      -- vim.pretty_print(depsets)
+      local extra_paths = {}
+      for _, depset in pairs(depsets) do
+        for extra_path in depset:gmatch('"(.-)"') do
+          if extra_path:match("^" .. ws_name) then
+            for _, pattern in pairs({ workspace, workspace .. "/bazel-bin" }) do
+              local path = extra_path:gsub("^" .. ws_name, pattern)
+              if Path:new(path):is_dir() then
+                extra_paths[path] = true
+              end
+            end
+          else
+            extra_paths[workspace .. "/external/" .. extra_path] = true
+          end
+        end
+      end
+      vim.schedule(function()
+        save_pyright_config_json(get_keys(extra_paths), include)
+      end)
+    end,
+  }):start()
 end
 
 function M.setup_pyright_with_bazel_for_this_target()
-  local workspace = bazel.get_workspace()
   vim.fn.BazelGetCurrentBufTarget()
-  add_python_deps_to_pyright(vim.g.current_bazel_target, workspace)
+  M.add_python_deps_to_pyright(vim.g.current_bazel_target)
 end
 
 function M.DebugBazel(type, bazel_config, get_program, args, get_env)
