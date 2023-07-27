4a73cd716737d8deec5cb15ec60520bb650a5d18
Alexander Born <alexander.born@bmw.de>
2023-07-27 10:44:31 +0200

    Fix vimwiki

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -7,7 +7,7 @@ return {
   { "mg979/vim-visual-multi" },
   { "LunarVim/bigfile.nvim" },
   { "davidgranstrom/nvim-markdown-preview" },
-  { "vimwiki/vimwiki", config = function() vim.g.vimwiki_list = { { ext = ".md", path = "~/projects/vimwiki/", syntax = "markdown", } } end, },
+  { "vimwiki/vimwiki", init = function() vim.g.vimwiki_list = { { ext = ".md", path = "~/projects/vimwiki/", syntax = "markdown", } } end, },
   { "aserowy/tmux.nvim", config = function() return require("tmux").setup({ copy_sync = { enable = false } }) end, },
   { "numToStr/Comment.nvim", config = function() require("Comment").setup() end, },
   { "gbprod/substitute.nvim", config = function() require("substitute").setup() end },
