68c5fe84f9d49fb3bc669da13a9e3112ef9877cc
Alexander Born <alexander.born@bmw.de>
2024-04-23 15:32:40 +0200

    Update formatting plugin

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,46 +1,13 @@
-local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name ~= "clangd" and client.name ~= "lua_ls"
-    end,
-    bufnr = bufnr,
-    timeout_ms = 2000,
-  })
-end
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        lsp_formatting(bufnr)
-      end,
-    })
-  end
-end
-
 return {
   {
-    "nvimtools/none-ls.nvim",
-    opts = function()
-      local null_ls = require("null-ls")
-      return {
-        sources = {
-          null_ls.builtins.formatting.black,
-          -- null_ls.builtins.formatting.isort,
-          -- null_ls.builtins.formatting.autoflake,
-          null_ls.builtins.formatting.buildifier,
-          null_ls.builtins.formatting.clang_format,
-          -- null_ls.builtins.formatting.json_tool,
-          -- null_ls.builtins.formatting.prettierd,
-          null_ls.builtins.formatting.stylua,
-        },
-        -- on_attach = on_attach,
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = {
+        cpp = { "clang-format" },
+        lua = { "stylua" },
+        python = { "black" },
+        bzl = { "buildifier" },
+        sh = { "shfmt" },
       }
     end,
   },
@@ -51,6 +18,7 @@ return {
       table.insert(opts.ensure_installed, "buildifier")
       table.insert(opts.ensure_installed, "clangd")
       table.insert(opts.ensure_installed, "black")
+      table.insert(opts.ensure_installed, "clang-format")
     end,
   },
 }
