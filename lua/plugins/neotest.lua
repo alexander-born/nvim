19e425214d7afccdf3c5b807592bdb0db1e19585
Alexander Born <alexander.born@bmw.de>
2024-04-23 15:34:22 +0200

    Update neotest config which added new dependency

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-neotest/neotest",
-    dependencies = { "antoinemadec/FixCursorHold.nvim", "nvim-neotest/neotest-python" },
+    dependencies = { "antoinemadec/FixCursorHold.nvim", "nvim-neotest/neotest-python", "nvim-neotest/nvim-nio" },
     config = function()
       local neotest = require("neotest")
       neotest.setup({
